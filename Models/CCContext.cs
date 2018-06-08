4a7b1e0aad0a127f6d43867bc0c12f5a2b5af9f9
marcos montiel <marcos-gab14@hotmail.com>
2018-06-08 17:27:23 -0500

    Marcos

---
+++ Models/CCContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cuidados.Caninos.Marcos.Montiel.Models
+{
+    public class CCContext : DbContext
+    {
+        public CCContext(DbContextOptions<CCContext> options)             : base(options)         {
+        }          public DbSet<ComCatEscolaridad> ComCatEscolaridad { get; set; }         public DbSet<ComCatSexo> ComCatSexo { get; set; }         public DbSet<ComPersona> ComPersona { get; set; }
+    }
+}
