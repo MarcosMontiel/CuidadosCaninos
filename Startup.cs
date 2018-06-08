2d837ab17b0c5f420dd7e52603dd1006f4202a73
marcos montiel <marcos-gab14@hotmail.com>
2018-06-08 17:30:48 -0500

    Marcos

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Cuidados.Caninos.Marcos.Montiel.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +20,9 @@ namespace Cuidados.Caninos.Marcos.Montiel
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<CCContext>(options =>
+                  options.UseSqlite("Data Source=CuidadosCaninos.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
