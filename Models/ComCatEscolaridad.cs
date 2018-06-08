b18deaa5eb8870910d22bb2c7843898f1a8c186c
marcos montiel <marcos-gab14@hotmail.com>
2018-06-08 17:09:28 -0500

    Marcos

---
+++ Models/ComCatEscolaridad.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cuidados.Caninos.Marcos.Montiel.Models
+{
+    public class ComCatEscolaridad
+    {
+        [Key]         public int ID { get; set; }          [Required]         [MaxLength(50)]         public string Nombre { get; set; }
+    }
+}
